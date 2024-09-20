844a7efde3e8e294cf67b8213dbb855e21985fd2
Kwonharu <byhank0@naver.com>
2024-09-20 16:25:50 +0900

    20240920162444

--- springtobi/src/main/java/com/example/tobi/springtobi/es1_4/Dao/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package com.example.tobi.springtobi.es1_4.Dao;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-  Connection makerNewConnection() throws SQLException, ClassNotFoundException;
-}
